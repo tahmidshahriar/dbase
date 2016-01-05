3751b2ae0f0a1adb665841e838935cff15ff5620
tahmidshahriar <tahmids@seas.upenn.edu>
2016-01-05 03:36:38 -0500

    yay

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return render_template('./app.html', form=form, myList = [])
 
+@app.route("/test")
+def hello():
+    return "hello"
+
 if __name__ == "__main__":
 	app.debug = True
 	app.secret_key = "hi"
