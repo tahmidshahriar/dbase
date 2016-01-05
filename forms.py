e0c3df4c94ee4d51ce39ee70159d08c1e111c2bc
tahmidshahriar <tahmids@seas.upenn.edu>
2016-01-05 04:43:44 -0500

    st

--- forms.py
+++ forms.py
@@ -1,4 +1,16 @@
 from flask.ext.wtf import Form
-from wtforms.fields import TextField
-class TodoForm(Form):
-    todo = TextField('todo')
+from wtforms.fields import TextField, PasswordField, DateField
+class DataForm(Form):
+    program = TextField('program')
+    eligibility = TextField('eligibility')
+    date = TextField('date')
+    applicationDeadline = TextField('applicationDeadline')
+    programHyper = TextField('programHyper')
+    applicationHyper = TextField('applicationHyper')
+    login = TextField('login')
+    courses = TextField('applicationDeadline')
+    faq = TextField('faq')
+    sat = TextField('sat')
+    lor = TextField('lor')
+    essay = TextField('essay')
+    note = TextField('note')
