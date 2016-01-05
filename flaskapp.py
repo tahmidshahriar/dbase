f96090c506ce6bdb43993aebf06d2b76cfac77eb
tahmidshahriar <tahmids@seas.upenn.edu>
2016-01-05 03:44:56 -0500

    yay

--- flaskapp.py
+++ flaskapp.py
@@ -8,15 +8,7 @@ app.config.from_pyfile('flaskapp.cfg')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/<path:resource>')
-def serveStaticResource(resource):
-    return send_from_directory('static/', resource)
-
-@app.route("/test")
-def test():
-    return "<strong>It's Alive!</strong>"
+	return render_template('app.html', form=form, myList = [])
 
 if __name__ == '__main__':
     app.run()
