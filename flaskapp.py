5e3c4967dd8675ae2bc72c94df1a01eb885139fd
tahmidshahriar <tahmids@seas.upenn.edu>
2016-01-05 04:35:20 -0500

    st

--- flaskapp.py
+++ flaskapp.py
@@ -25,7 +25,7 @@ def index():
             return redirect('/')
     else:
         try:
-            conn=pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_URL'])
+            conn = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_URL'])
             return "Hi"
             db = conn.test
             todo = db.todo
